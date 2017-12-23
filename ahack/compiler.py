ffa8d051aa5fad86cdef481200a5a91eec8d3fe3
vkhristenko <vdkhristenko1991@gmail.com>
2017-12-23 04:12:48 +0300

    Assembly process

--- ahack/compiler.py
+++ ahack/compiler.py
@@ -10,11 +10,35 @@ class Compiler(object):
     def compile(self, path):
         """
         Compile the provided assembly file and generate the Hack Machine Code
+
+        The Assembly Process:
+        1. Initialization
+            - Construct an empty symbol table
+            - Add pre-defined symbols
+        2. First Pass
+            - Scan the entire source file
+            - For each instruction of the form (XXX)
+                -> Add the pair (xxx, address) to the symbol table, where
+                address is the number of the instruction following (XXX)
+        3. Second Passs
+            - Set n to 16
+            - Scan the entire program and for each instruction:
+                -> If (symbol, value) is found, use value to complete the instructions
+                    translation.
+                -> If not found
+                    - Add (symbol, n) to the SymbolTable
+                    - Use n to complete the instruction's translation
+                    - n++
+            - If the instruction is a C-instruction, complete the instruction's 
+                translation
+            - Write the translated instruction to he output file
         """
         pass
 #        with Parser(path) as p:
-            # fetch the next instruction
+#            # fetch the next instruction
 #            inst = p.next()
+#            # for each instruction, 
+#            while inst is not None:
 
     def interpret(self, s):
         """
